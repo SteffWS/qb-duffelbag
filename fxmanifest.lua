b3ed6d854a13390f03b2c3bbc632df42c50dae91
Steff <84989476+SteffWS@users.noreply.github.com>
2024-05-20 17:41:24 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author '@steff'
+description 'A item-stashing pack'
+version '1.0.0'
+
+client_scripts {	
+	'client/cl_main.lua'
+}
+
+server_scripts {	
+	'server/sv_main.lua'
+}
